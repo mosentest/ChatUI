3c01b4429ae9e7b1c0be839f5c23407ad1538214
ziqi mo <709847739@qq.com>
2021-02-18 15:34:13 +0800

    Initial commit

---
+++ app/src/main/java/com/rance/chatui/adapter/holder/BaseViewHolder.java
@@ -0,0 +1,22 @@
+package com.rance.chatui.adapter.holder;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by chengz
+ *
+ * @date 2017/8/3.
+ */
+
+public class BaseViewHolder<M> extends RecyclerView.ViewHolder {
+
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    public void setData(M data) {
+
+    }
+}
